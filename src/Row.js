1b5bdf22168ddbc75c95b01e26471a68620aae1a
wenner <nelsondiasdemedeiros@gmail.com>
2020-06-18 17:34:38 -0300

    feat: create component Row

---
+++ src/Row.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 0.5rem;
+`;
+
+export default Row;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export { default as Container } from './Container';
 export { default as FullContainer } from './FullContainer';
+export { default as Container } from './Container';
+export { default as Row } from './Row';
